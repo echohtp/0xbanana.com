20db85aa0ce1741a9319787b35edf0ea12c9b42b
Jason Schorr <jason@Jasons-MacBook-Pro-2.local>
2023-10-13 23:08:48 -0400

    update idnex

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -307,10 +307,7 @@ export default function Home({ articles }) {
             Engineer, Artist, Solana Enjooyer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m banana, an artist, a software engineer and entrepreneur based in web3. 
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
